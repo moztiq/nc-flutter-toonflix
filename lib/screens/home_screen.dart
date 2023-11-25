f832c55c7dfd89671c550dc779b2960938a1a3b1
Seongki Kim <moztiq@gmail.com>
2023-11-25 15:55:35 +0900

    fromJson & waitForWebtoons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:nc_flutter_toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../models/webtoon_model.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
+    
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,24 @@
+import "dart:convert";
+
 import "package:http/http.dart" as http;
+import "package:nc_flutter_toonflix/models/webtoon_model.dart";
 
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
-  final String today = 'today';
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = 'today';
 
-  void getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      print(webtoonInstances);
+      return webtoonInstances;
     }
 
     throw Error();
