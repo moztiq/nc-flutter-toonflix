ff1bfc10eab7ddfc940559c18bc2db5c0e854955
Seongki Kim <moztiq@gmail.com>
2023-11-26 11:59:40 +0900

    Hero

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -33,25 +33,28 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 10,
-                      offset: const Offset(5, 5),
-                      color: Colors.black.withOpacity(0.5),
-                    ),
-                  ],
-                ),
-                clipBehavior: Clip.hardEdge,
-                width: 200,
-                child: Image.network(
-                  webtoon.thumb,
-                  headers: const {
-                    "User-Agent":
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                  },
+              Hero(
+                tag: webtoon.id,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        offset: const Offset(5, 5),
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ],
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  width: 200,
+                  child: Image.network(
+                    webtoon.thumb,
+                    headers: const {
+                      "User-Agent":
+                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    },
+                  ),
                 ),
               ),
             ],
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -19,31 +19,34 @@ class Webtoon extends StatelessWidget {
           context,
           MaterialPageRoute(
             builder: (context) => DetailScreen(webtoon: webtoon),
-            fullscreenDialog: true,
+            // fullscreenDialog: true,
           ),
         );
       },
       child: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 10,
-                  offset: const Offset(5, 5),
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              ],
-            ),
-            clipBehavior: Clip.hardEdge,
-            width: 200,
-            child: Image.network(
-              webtoon.thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+          Hero(
+            tag: webtoon.id,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    offset: const Offset(5, 5),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              clipBehavior: Clip.hardEdge,
+              width: 200,
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
           ),
           const SizedBox(
