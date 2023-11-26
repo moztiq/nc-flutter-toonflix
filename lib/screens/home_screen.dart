4d78ae64e59a40e5813c28b62c0202ccf2ebe8d0
Seongki Kim <moztiq@gmail.com>
2023-11-26 10:44:05 +0900

    ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,11 +6,10 @@ import '../models/webtoon_model.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
@@ -28,9 +27,21 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text("There is data!");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => SizedBox(
+                width: 20,
+              ),
+            );
           }
-          return Text("Loading...");
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
